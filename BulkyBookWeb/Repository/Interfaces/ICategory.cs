e7b7a1dfb02c39b5392cbdee6ca81c6f5a848d8c
Md. Sajjat Hossen <sajjat.hossen@brainstation-23.com>
2024-05-12 14:07:41 +0600

    refactoring using repositiory pattern

---
+++ BulkyBookWeb/Repository/Interfaces/ICategory.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Repository.Interfaces
+{
+    public interface ICategory
+    {
+        Task<IEnumerable<ICategory>> GetAll();
+        Task AddCategory(Category category);
+        Task<Category> FindCategoyByID(int id);
+        Task UpdateCategory(Category category);
+        Task RemoveCategory(int id);
+
+    }
+}
---
+++ BulkyBookWeb/Repository/Services/CategoryService.cs
@@ -0,0 +1,72 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using BulkyBookWeb.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BulkyBookWeb.Repository.Services
+{
+    public class CategoryService : ICategory
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        #region AddCategory
+
+        public async Task AddCategory(Category category)
+        {
+            await db.Categories.AddAsync(category);
+            await db.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region RemoveCategory
+
+        public async Task RemoveCategory(int id)
+        {
+            var category = this.FindCategoyByID(id);
+            db.Categories.Remove(await category);
+            await db.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region FindCategoryByID
+
+        public async Task<Category> FindCategoyByID(int id)
+        {
+            var category = await db.Categories.FindAsync(id);
+
+            return category;
+        }
+
+        #endregion
+
+        #region GetAll
+
+        public async Task<IEnumerable<ICategory>> GetAll()
+        {
+            IEnumerable<ICategory> data = (IEnumerable<ICategory>)await db.Categories.ToListAsync();
+
+            return data;
+        }
+
+        #endregion
+
+        #region UpdateCategory
+
+        public async Task UpdateCategory(Category category)
+        {
+            db.Categories.Update(category);
+            await db.SaveChangesAsync();
+        }
+
+        #endregion
+
+    }
+}
